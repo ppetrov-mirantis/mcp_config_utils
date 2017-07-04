e8edaafcb366ac54b8efdd056d4b6a481d343d1d
Pavel Petrov <ppetrov@mirantis.com>
2017-07-04 17:15:42 +0300

    OVS config script was optimized

--- networking/turn_ovs_on.sh
+++ networking/turn_ovs_on.sh
@@ -1,17 +1,12 @@
-# Reconfigure cmp-nodes
-salt 'cmp*' cmd.run "sed -i 's/firewall_driver = neutron.agent.linux.iptables_firewall.OVSHybridIptablesFirewallDriver/\
-firewall_driver = openvswitch/g' /etc/neutron/plugins/ml2/openvswitch_agent.ini"
-salt 'cmp*' service.restart "neutron-openvswitch-agent"
-# check results
-salt 'cmp*' cmd.run "grep -r 'firewall_driver' /etc/neutron/* | grep -v '#'"
+# get configuration state before reconfiguration process
+echo "Configuration STATE BEFORE reconfiguration procedure:"
+salt -E 'cmp*|ctl*|gtw*' cmd.run "grep -r 'firewall_driver' /etc/neutron/* | grep -v '#'"
 
 
-# Reconfigure gtw-nodes
-salt 'gtw*' cmd.run "sed -i 's/firewall_driver = neutron.agent.linux.iptables_firewall.OVSHybridIptablesFirewallDriver/\
+# Reconfigure cmp- and gtw-nodes 
+salt -E 'cmp*|gtw*' cmd.run "sed -i 's/firewall_driver = neutron.agent.linux.iptables_firewall.OVSHybridIptablesFirewallDriver/\
 firewall_driver = openvswitch/g' /etc/neutron/plugins/ml2/openvswitch_agent.ini"
-salt 'gtw*' service.restart "neutron-openvswitch-agent"
-# check results
-salt 'gtw*' cmd.run "grep -r 'firewall_driver' /etc/neutron/* | grep -v '#'"
+salt -E 'cmp*|gtw*' service.restart "neutron-openvswitch-agent"
 
 
 # Reconfigure ctl-nodes
@@ -20,5 +15,8 @@ firewall_driver = openvswitch/g' /etc/neutron/plugin.ini"
 salt 'ctl*' cmd.run "sed -i 's/firewall_driver = neutron.agent.linux.iptables_firewall.OVSHybridIptablesFirewallDriver/\
 firewall_driver = openvswitch/g' /etc/neutron/plugins/ml2/ml2_conf.ini"
 salt 'ctl*' service.restart "neutron-server"
+
+
 # check results
-salt 'ctl*' cmd.run "grep -r 'firewall_driver' /etc/neutron/* | grep -v '#'"
+echo "Configuration STATE AFTER reconfiguration procedure:"
+salt -E 'cmp*|ctl*|gtw*' cmd.run "grep -r 'firewall_driver' /etc/neutron/* | grep -v '#'"
